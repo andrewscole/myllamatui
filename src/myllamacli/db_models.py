b5868c8b47abd8922cebe093e513caae55a79f25
Andrew Cole <376180+andrewscole@users.noreply.github.com>
2025-05-02 18:05:45 -0500

    Updated test_db_model and pyproject. Also ensured a few paths

--- src/myllamacli/db_models.py
+++ src/myllamacli/db_models.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 from peewee import *
 from datetime import datetime
 
-from myllamacli.init_files import set_database_path
+from src.myllamacli.init_files import set_database_path
 
 MYLLAMACLI_DB = set_database_path()
 
--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -1,8 +1,34 @@
+import pytest
+
 from src.myllamacli.db_models import Context, Topic, Category, LLM_MODEL, Chat, CLI_Settings
 
 
 
 def test_Context_creation(test_database):
-    context = Context.create(text="test context")
+    Context.create(text="test context")
     result = Context.get_by_id(1)
     assert result.text == "test context"
+
+def test_Category_creation(test_database):
+    Category.create(text="test category")
+    result = Category.get_by_id(1)
+    assert result.text == "test category"
+
+
+def test_Topic_creation(test_database):
+    category_test = Category.create(text="test category")
+    Topic.create(text="test topic", category_id=category_test.id)
+    cat_id = category_test.id
+    result = Topic.get_by_id(1)
+    print(result.id)
+    assert result.text == "test topic"
+    # this is kind of stupid and probably not a great test
+    assert str(result.category_id) == str(cat_id)
+
+def test_LLM_MODEL_creation(test_database):
+    context = LLM_MODEL.create(model="MyFakeModel", size=34567, specialization="general", currently_available=True)
+    result = LLM_MODEL.get_by_id(1)
+    assert result.model == "MyFakeModel"
+    assert result.size == 34567
+    assert result.specialization == "general"
+    assert result.currently_available == True
